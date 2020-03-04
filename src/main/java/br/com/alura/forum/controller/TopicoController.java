e0d5c2beced4d3aba3cb7fcff47c97ef855b9253
luccastraumer <cc19372@g.unicamp.br>
2020-03-04 14:27:53 -0300

    Annotations,Repository, Spring Data JPA and Database

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -4,21 +4,32 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @Controller
 public class TopicoController {
 	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
 	@RequestMapping("topicos")
 	@ResponseBody
-	public List<Topico> lista(){
-		Topico topico = new Topico("Duvida","Duvida com Spring",new Curso("Spring","Programacao"));
-		return Arrays.asList(topico,topico,topico);
+	public List<TopicoDto> lista(String nomeCurso){
+		if(nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		}else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
 	}
 
 }
